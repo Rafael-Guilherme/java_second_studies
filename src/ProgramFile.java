888db139fe3e29b3f693a9b0784a03fa46ca4f47
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-02-26 16:04:34 -0300

    try with resources

--- src/ProgramFile.java
+++ src/ProgramFile.java
@@ -6,13 +6,8 @@ public class ProgramFile {
     public static void main(String[] args) {
         
         String path = "/home/rafael/Área de Trabalho/Java/Aulas/primeiro_programa/second_programm/arquivo.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while(line != null) {
@@ -21,17 +16,6 @@ public class ProgramFile {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        } 
     }
 }
