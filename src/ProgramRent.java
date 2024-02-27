2a2408d69358573ac5b71009d10c13c635297bff
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-02-27 10:19:47 -0300

    program without interface

---
+++ src/ProgramRent.java
@@ -0,0 +1,46 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.CarRental;
+import entities.Vehicle;
+import services.BrazilTaxService;
+import services.RentalService;
+
+public class ProgramRent {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com os dados do aluguel");
+        System.out.print("Modelo do carro: ");
+        String carModel = sc.nextLine();
+        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.println("Devolução (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.print("Entre o preço por hora: ");
+        double pricerPerHour = sc.nextDouble();
+        System.out.print("Entre o preço por dia: ");
+        double pricerPerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricerPerDay, pricerPerHour, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println();
+        System.out.println("FATURA:");
+        System.out.println("Pagamento básico: " + cr.getInvoice().getBasicPayment());
+        System.out.println("Imposto: " + cr.getInvoice().getTax());
+        System.out.println("Pagamento total: " + cr.getInvoice().getTotalPayment());
+
+        sc.close();
+    }
+}
---
+++ src/services/RentalService.java
@@ -0,0 +1,37 @@
+package services;
+
+import java.time.Duration;
+
+import entities.CarRental;
+import entities.Invoice;
+
+public class RentalService {
+    
+    private Double pricePerDay;
+    private Double pricerPerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricerPerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricerPerHour = pricerPerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60;
+
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = pricerPerHour * Math.ceil(hours);
+        } else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
