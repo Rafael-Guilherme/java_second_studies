952747399acccb090045eefafa553fd2e5fdc5d0
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-02-28 15:20:39 -0300

    multiplas interfaces

---
+++ src/ProgramDevice.java
@@ -0,0 +1,23 @@
+import entities.ComboDevice;
+import entities.ConcretePrinter;
+import entities.ConcreteScanner;
+
+public class ProgramDevice {
+    public static void main(String[] args) {
+
+        ConcretePrinter p = new ConcretePrinter("1080");
+        p.processDoc("My letter");
+        p.print("My letter");
+
+        System.out.println();
+        ConcreteScanner s = new ConcreteScanner("2003");
+        s.processDoc("My Email");
+        System.out.println("Scan result: " + s.scan());
+
+        System.out.println();
+        ComboDevice c = new ComboDevice("2081");
+        c.processDoc("My thesis");
+        c.print("My thesis");
+        System.out.println("Scan result: " + c.scan());
+    }
+}
